a151307744ef3e1aea4460db88549e1ea1c98199
teu <daoquyanh2000@gmail.com>
2021-11-09 16:47:46 +0700

    push ví dụ lên

---
+++ assignment02-1/DIPF2103B18DCDT003AS10221.cpp
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////
+//// 
+//// ma nhom btl:		23
+//// ten mon hoc:     xu ly anh
+//// thong tin sv1:   Dao Quy Anh         - B18DCDT003    - 03 - B18CQDT03-B - daoquyanh13700@gmail.com
+//// thong tin sv2:   Dang Hoang Giang    - B18DCDT059    - 04 - B18CQDT03-B - dgiang69@gmail.com
+//// thong tin sv3:   Nguyen Huy Hung     - B18DCDT091    - 04 - B18CQDT03-B - nhuyhung146@gmail.com
+//// 
+///////////////////////////////////////////////////
+//#include "opencv2/imgcodecs.hpp"
+//#include "opencv2/highgui.hpp"
+//#include "opencv2/imgproc.hpp"
+//#include <iostream>
+//using namespace cv;
+//using namespace std;
+//int main(int argc, char** argv)
+//{
+//    CommandLineParser parser(argc, argv, "{@input | lena.jpg | input image}");
+//    Mat src = imread(samples::findFile(parser.get<String>("@input")), IMREAD_COLOR);
+//    if (src.empty())
+//    {
+//        cout << "Could not open or find the image!\n" << endl;
+//        cout << "Usage: " << argv[0] << " <Input image>" << endl;
+//        return -1;
+//    }
+//    cvtColor(src, src, COLOR_BGR2GRAY);
+//    Mat dst;
+//    equalizeHist(src, dst);
+//    imshow("Source image", src);
+//    imshow("Equalized Image", dst);
+//    waitKey();
+//    return 0;
+//}
